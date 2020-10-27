5bc736b243bf044c050804f227fef8b1ea67daf0
kadeshiro <gaaadaaa16@gmail.com>
2020-10-28 00:25:32 +0900

    fix functionsの実行をホストで制限

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,30 +6,38 @@ const runtimeOpts = {
   memory: "1GB",
 } as const
 
+const appHost = "gen-resume.netlify.app"
+
 export const getPdf = functions
   .region("asia-northeast1")
   .runWith(runtimeOpts)
   .https.onRequest(async (request, response) => {
+    setHeader(response, appHost)
+    if (request.hostname !== appHost) {
+      response.status(403).end()
+      return
+    }
+
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.goto(`data:text/html,${request.body.content}`, {
       waitUntil: "networkidle0",
     })
-    await page.evaluate(() => {
-      const font = window.getComputedStyle(document.body).fontFamily
-      const p = document.createElement("p")
-      p.style.fontSize = "30px"
-      p.innerText = font
-      document.body.append(p)
-    })
     await page.emulateMediaType("screen")
+
     const pdf = await page.pdf({
       format: "A4",
       printBackground: true,
     })
+
     await browser.close()
-    response.setHeader("Content-Type", "application/pdf")
-    response.setHeader("Access-Control-Allow-Origin", "*")
-    response.setHeader("Access-Control-Allow-Headers", "*")
+
     response.send(pdf)
   })
+
+const setHeader = (response: functions.Response, host: string) => {
+  response.setHeader("Content-Type", "application/pdf")
+  response.setHeader("Access-Control-Allow-Origin", `https://${host}/`)
+  response.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
+  response.setHeader("Access-Control-Allow-Headers", "Content-Type")
+}
